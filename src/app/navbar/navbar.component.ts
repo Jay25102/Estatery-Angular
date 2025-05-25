42110c38df9b8a2999474ebd514b374724ff31c2
jay25102 <jasonhuang25102@gmail.com>
2025-05-25 17:23:54 -0400

    adding text and images to navbar and hero

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { matKeyboardArrowDownRound } from '@ng-icons/material-icons/round'
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgIcon],
+  viewProviders: [provideIcons({matKeyboardArrowDownRound})],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
