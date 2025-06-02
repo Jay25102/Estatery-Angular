607e8bb33476c7751cc5d623cc3368920493cf97
jay25102 <jasonhuang25102@gmail.com>
2025-06-02 15:55:47 -0400

    finished hero css desktop and mobile view

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -2,12 +2,12 @@ import { Component, computed, inject, input, signal } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { CardsizesService } from "../services/cardsizes.service";
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { matCalendarMonthRound } from '@ng-icons/material-icons/round';
+import { matCalendarMonthRound, matSearchRound } from '@ng-icons/material-icons/round';
 
 @Component({
   selector: 'app-hero',
   imports: [CardComponent, NgIcon],
-  viewProviders: [provideIcons({matCalendarMonthRound})],
+  viewProviders: [provideIcons({matCalendarMonthRound, matSearchRound})],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
