ec13f892b851b9b78812c24da1bc0b027b997e0e
jay25102 <jasonhuang25102@gmail.com>
2025-06-03 16:40:27 -0400

    footer grid jank inbound

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,5 +7,64 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  footerLinks = [
+    {
+      id: 1,
+      heading: 'SELL A HOME',
+      links: [
+        'Request and offer',
+        'Pricing',
+        'Reviews',
+        'Stories'
+      ]
+    },
+    {
+      id: 2,
+      heading: 'BUY A HOME',
+      links: [
+        'Buy',
+        'Finance',
+      ]
+    },
+    {
+      id: 3,
+      heading: 'BUY, RENT, AND SELL',
+      links: [
+        'Buy & sell properties',
+        'Rent home',
+        'Builder trade-up',
+      ]
+    },
+    {
+      id: 4,
+      heading: 'TERMS & PRIVACY',
+      links: [
+        'Trust & Safety',
+        'Terms of Service',
+        'Privacy Policy',
+      ]
+    },
+    {
+      id: 5,
+      heading: 'ABOUT',
+      links: [
+        'Company',
+        'How it works',
+        'Contact',
+        'Investors',
+      ]
+    },
+    {
+      id: 6,
+      heading: 'Resources',
+      links: [
+        'Blog',
+        'Guides',
+        'FAQ',
+        'Help Center',
+      ]
+    },
+  ]
 
+  desktopView: boolean = true;
 }
